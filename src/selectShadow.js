dab5c65238e3067ad0adb80903d11913c1e0c620
christophe-g <christophe.geiser@gmail.com>
2019-09-05 10:47:20 +0200

    back to normal import

--- src/selectShadow.js
+++ src/selectShadow.js
@@ -1,7 +1,8 @@
 import {
   select
 }
-from '/web-modules/d3-selection.js';
+from 'd3-selection';
+// from '/web-modules/d3-selection.js';
 
 const selectShadow = (selector, el) => {
   return select(el.shadowRoot.querySelector(selector));
