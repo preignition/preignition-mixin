2282b37d349597bc7a84c49b2b9a41a5a0dedb6a
christophe-g <christophe.geiser@gmail.com>
2019-09-18 11:57:46 +0200

    add doNotSetUndefinedValue mixin

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export { default as defaultValueMixin } from './src/defaultValueMixin.js';
 export { default as selectMixin } from './src/selectMixin.js';
 export { default as selectShadow } from './src/selectShadow.js';
 export { default as cssTheme } from './src/cssTheme.js';
+export { default as doNotSetUndefinedValue } from './src/doNotSetUndefinedValue.js'; 
