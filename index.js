09e94c0aeaf6769a74f7ce946d41c589763c0fc9
christophe-g <christophe.geiser@gmail.com>
2019-09-26 09:32:57 +0200

    capitalized doNotSetUndefinedValues as it is a mixin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export { default as selectShadow } from './src/selectShadow.js';
 export { default as cssTheme } from './src/cssTheme.js';
-export { default as doNotSetUndefinedValue } from './src/doNotSetUndefinedValue.js'; 
 
+export { default as DoNotSetUndefinedValue } from './src/doNotSetUndefinedValue.js'; 
 export { default as SelectMixin } from './src/selectMixin.js';
 export { default as DefaultValueMixin } from './src/defaultValueMixin.js';
 export { default as RelayTo } from './src/relayToMixin.js';
